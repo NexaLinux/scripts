3c7ae903bb3e68d68c9f805917a2c43d133190dc
komaru <winbo.yml@proton.me>
2025-02-06 16:48:28 +0100

    [scripts] Last fix up before ISO ready'

--- iso.sh
+++ iso.sh
@@ -11,14 +11,15 @@ git clone https://github.com/pieroproietti/get-eggs /tmp/prod/get-eggs/
 cd /tmp/prod/get-eggs/
 sudo ./get-eggs.sh
 
-echo "[ISO] Cloning Calamares theme"
-git clone https://github.com/NexaLinux/calamares /tmp/prod/calamares/
+echo "[ISO] Adding Calamares theme"
+rm -rf /usr/lib/penguins-eggs/addons/eggs/theme/calamares/branding/
+git clone https://github.com/NexaLinux/calamares /usr/lib/penguins-eggs/addons/eggs/theme/calamares/branding
 
 echo "[ISO] Installing Calamares (eggs)"
-sudo eggs calamares --install --theme=/tmp/prod/calamares/
+sudo eggs calamares --install
 
 echo "[ISO] Running configuration"
 sudo eggs dad
 
 echo "[ISO] Producing an egg!"
-sudo eggs produce --excludes homes --release --theme=/tmp/prod/calamares/
+sudo eggs produce --excludes homes --release --clone --noicon
