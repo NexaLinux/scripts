5d7f8248416deb56537def69b42494aacbfb360f
komaru <winbo.yml@proton.me>
2025-02-06 12:20:59 +0100

    [scripts] Fix up

--- iso.sh
+++ iso.sh
@@ -8,16 +8,17 @@ mkdir /tmp/prod
 
 echo "[ISO] Installing Penguins' eggs"
 git clone https://github.com/pieroproietti/get-eggs /tmp/prod/get-eggs/
-sudo /tmp/prod/get-eggs/get-eggs.sh
+cd /tmp/prod/get-eggs/
+sudo ./get-eggs.sh
 
 echo "[ISO] Cloning Calamares theme"
 git clone https://github.com/NexaLinux/calamares /tmp/prod/calamares/
 
 echo "[ISO] Installing Calamares (eggs)"
-sudo eggs calamares --install --theme=/tmp/prod/calamares
+sudo eggs calamares --install --theme=/tmp/prod/calamares/
 
 echo "[ISO] Running configuration"
 sudo eggs dad
 
 echo "[ISO] Producing an egg!"
-sudo eggs produce --excludes homes --release --theme=/tmp/prod/calamares
+sudo eggs produce --excludes homes --release --theme=/tmp/prod/calamares/
