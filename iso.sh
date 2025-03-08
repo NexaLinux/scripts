191e63ab4c5deedd48975444fe109d33185015c2
komaru.yml <170887716+komaru-meow@users.noreply.github.com>
2025-03-08 15:42:24 +0100

    [scripts] [iso] Clone? I WANT A NON-EXISTING USER! Crypted clone? I WILL BREAK!

--- iso.sh
+++ iso.sh
@@ -25,4 +25,4 @@ echo "[ISO] Running configuration"
 sudo eggs dad
 
 echo "[ISO] Producing an egg!"
-sudo eggs produce --theme nexa --release --noicon --clone
+sudo eggs produce --theme nexa --release --noicon --cryptedclone
